b633c1d8de2d8beeb0ccf64eed0c418caabbc089
Hilmi FC <hilmie123013@gmail.com>
2023-05-20 17:05:05 +0700

    commit-contoh

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,14 +5,16 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
+
+		counter.increaseBy(2);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.increment();
+		counter.decreaseBy(4);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.decrement();
+		counter.multiplyBy(6);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
